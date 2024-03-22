5a240175778a502c1a9db188967d00c0b10e1446
kenedy4 <kennedymusyoki8@gmail.com>
2024-03-22 16:58:18 +0300

    committing changes to functional arrays

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+{
+  function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2, 3 + 1);
+  }
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
+  function selectingDrivers() {
+    return [returnFirstTwoDrivers, returnLastTwoDrivers];
+  }
+  //   const selectedDrivers = selectingDrivers();
+}
+
+//   const selectingDrivers = ["returnFirstTwoDrivers", "returnLastTwoDrivers"];
+//   {
+//     return selectingDrivers(drvers);
+//   }
+// }
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+function fareTripler(fare) {
+  return fare * 3;
+}
